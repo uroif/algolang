63d7513403a67c64d9f356584b1075b315e2e358
uroif <37129905+uroif@users.noreply.github.com>
2018-12-10 10:55:30 +0700

    Update labiec06.go

--- day4/labiec06.go
+++ day4/labiec06.go
@@ -7,9 +7,13 @@ func main() {
 
 	fmt.Scanf("%d", &n)
 
-	for i := 2; i < n/2; i++ {
-		if n % i == 0 && i < n/i {
-			fmt.Printf("%d %d ", i, n/i)
+	for i := 1; i <= n/2; i++ {
+		if n % i == 0 && i <= n/i {
+			if i != n/i && i != 1 {
+				fmt.Printf("%d %d ", i, n/i)
+			} else {
+				fmt.Printf("%d ", i)
+			}
 		}
 	}
 }
