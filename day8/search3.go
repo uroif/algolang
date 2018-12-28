5b1743b39ab3843d8427c34491e0fbf1a1626f5e
uroif <fiorume@gmail.com>
2018-12-28 23:09:14 +0700

    finish

--- day8/search3.go
+++ day8/search3.go
@@ -3,6 +3,7 @@ package main
 import (
 	"algolang/lib"
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -16,7 +17,12 @@ func main() {
 		fmt.Scan(&n, &x)
 
 		slc := lib.InputSlice(n)
-		fmt.Println(search(slc, x))
+		if len(search(slc, x)) == 0 {
+			fmt.Println("-1")
+		} else {
+			slc = search(slc, x)
+			fmt.Println(strings.Trim(strings.Replace(fmt.Sprint(slc), " ", " ", -1), "[]"))
+		}
 	}
 }
 
@@ -24,7 +30,7 @@ func search(slc []int, x int) []int {
 	var rsl []int
 	for i, v := range slc {
 		if v == x {
-			rsl = append(rsl, i)
+			rsl = append(rsl, i+1)
 		}
 	}
 	return rsl
